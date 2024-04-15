4f357f6f896314212fafe9690b60d32d7efcd467
nylonmusk <2912rla@gmail.com>
2024-04-15 10:15:33 +0900

    feat : AutoCompleteService 생성 및 역할 분리

--- demo/src/main/java/com/example/searchAPI/controller/AutoCompleteController.java
+++ demo/src/main/java/com/example/searchAPI/controller/AutoCompleteController.java
@@ -1,102 +1,24 @@
 package com.example.searchAPI.controller;
 
-import com.example.searchAPI.config.ElasticConfiguration;
-import com.example.searchAPI.constant.autocomplete.Option;
-import org.elasticsearch.action.search.SearchRequest;
-import org.elasticsearch.action.search.SearchResponse;
-import org.elasticsearch.client.RequestOptions;
-import org.elasticsearch.index.query.QueryBuilders;
-import org.elasticsearch.search.SearchHit;
-import org.elasticsearch.search.builder.SearchSourceBuilder;
-import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
-import org.elasticsearch.search.fetch.subphase.highlight.HighlightField;
-import org.springframework.beans.factory.annotation.Value;
+import com.example.searchAPI.service.AutoCompleteService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
-import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/autocomplete")
 public class AutoCompleteController {
 
-    @Value("${autocomplete.index}")
-    private String autocompleteIndex;
-
-    @Value("${autocomplete.field}")
-    private String field;
-
-    @Value("${search.host}")
-    private String host;
-
-    @Value("${search.port}")
-    private int port;
-
-    @Value("${search.username}")
-    private String username;
-
-    @Value("${search.protocol}")
-    private String protocol;
+    @Autowired
+    private AutoCompleteService autoCompleteService;
 
     @GetMapping("/")
     public List<String> autoComplete(@RequestParam(value = "keyword") String keyword,
                                      @RequestParam(value = "option") String option) {
-        try (ElasticConfiguration elasticConfiguration = new ElasticConfiguration(host, port, username, protocol)) {
-            SearchRequest searchRequest = new SearchRequest(autocompleteIndex);
-            SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
-
-            HighlightBuilder highlightBuilder = new HighlightBuilder();
-            highlightBuilder.field(field);
-            highlightBuilder.preTags("<em>");
-            highlightBuilder.postTags("</em>");
-            searchSourceBuilder.highlighter(highlightBuilder);
-
-            getQueryBuilder(keyword, option, searchSourceBuilder, field);
-
-            searchRequest.source(searchSourceBuilder);
-
-            SearchResponse searchResponse = elasticConfiguration.getElasticClient().search(searchRequest, RequestOptions.DEFAULT);
-            Map<String, Integer> frequencyMap = new HashMap<>();
-            Pattern pattern = Pattern.compile("<em>(.*?)</em>");
-
-            for (SearchHit hit : searchResponse.getHits().getHits()) {
-                HighlightField highlightField = hit.getHighlightFields().get(field);
-                if (highlightField != null) {
-                    String text = highlightField.fragments()[0].string();
-                    Matcher matcher = pattern.matcher(text);
-                    while (matcher.find()) {
-                        String matched = matcher.group(1);
-                        frequencyMap.put(matched, frequencyMap.getOrDefault(matched, 0) + 1);
-                    }
-                }
-            }
-
-            return frequencyMap.entrySet().stream()
-                    .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
-                    .map(Map.Entry::getKey)
-                    .collect(Collectors.toList());
-        } catch (IOException | IllegalArgumentException e) {
-            List<String> errorMessages = new ArrayList<>();
-            errorMessages.add(e.getMessage());
-            return errorMessages;
-        }
-    }
-
-    private static void getQueryBuilder(String keyword, String option, SearchSourceBuilder searchSourceBuilder, String field) {
-        if (option.equals(Option.PREFIX.get())) {
-            searchSourceBuilder.query(QueryBuilders.prefixQuery(field, keyword));
-        } else if (option.equals(Option.SUFFIX.get())) {
-            searchSourceBuilder.query(QueryBuilders.wildcardQuery(field, "*" + keyword));
-        } else if (option.equals(Option.CONTAINS.get())) {
-            searchSourceBuilder.query(QueryBuilders.wildcardQuery(field, "*" + keyword + "*"));
-        } else {
-            throw new IllegalArgumentException("정확한 option을 입력하세요.");
-        }
+        return autoCompleteService.autoComplete(keyword, option);
     }
 }
---
+++ demo/src/main/java/com/example/searchAPI/service/AutoCompleteService.java
@@ -0,0 +1,100 @@
+package com.example.searchAPI.service;
+
+import com.example.searchAPI.config.ElasticConfiguration;
+import com.example.searchAPI.constant.autocomplete.Option;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightField;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+@Service
+public class AutoCompleteService {
+
+    @Value("${autocomplete.index}")
+    private String autocompleteIndex;
+
+    @Value("${autocomplete.field}")
+    private String field;
+
+    @Value("${search.host}")
+    private String host;
+
+    @Value("${search.port}")
+    private int port;
+
+    @Value("${search.username}")
+    private String username;
+
+    @Value("${search.protocol}")
+    private String protocol;
+
+
+    public List<String> autoComplete(String keyword, String option) {
+        try (ElasticConfiguration elasticConfiguration = new ElasticConfiguration(host, port, username, protocol)) {
+            SearchRequest searchRequest = new SearchRequest(autocompleteIndex);
+            SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+
+            HighlightBuilder highlightBuilder = new HighlightBuilder();
+            highlightBuilder.field(field);
+            highlightBuilder.preTags("<em>");
+            highlightBuilder.postTags("</em>");
+            searchSourceBuilder.highlighter(highlightBuilder);
+
+            getQueryBuilder(keyword, option, searchSourceBuilder, field);
+
+            searchRequest.source(searchSourceBuilder);
+
+            SearchResponse searchResponse = elasticConfiguration.getElasticClient().search(searchRequest, RequestOptions.DEFAULT);
+            Map<String, Integer> frequencyMap = new HashMap<>();
+            Pattern pattern = Pattern.compile("<em>(.*?)</em>");
+
+            for (SearchHit hit : searchResponse.getHits().getHits()) {
+                HighlightField highlightField = hit.getHighlightFields().get(field);
+                if (highlightField != null) {
+                    String text = highlightField.fragments()[0].string();
+                    Matcher matcher = pattern.matcher(text);
+                    while (matcher.find()) {
+                        String matched = matcher.group(1);
+                        frequencyMap.put(matched, frequencyMap.getOrDefault(matched, 0) + 1);
+                    }
+                }
+            }
+
+            return frequencyMap.entrySet().stream()
+                    .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
+                    .map(Map.Entry::getKey)
+                    .collect(Collectors.toList());
+        } catch (IOException | IllegalArgumentException e) {
+            List<String> errorMessages = new ArrayList<>();
+            errorMessages.add(e.getMessage());
+            return errorMessages;
+        }
+    }
+
+    private static void getQueryBuilder(String keyword, String option, SearchSourceBuilder searchSourceBuilder, String field) {
+        if (option.equals(Option.PREFIX.get())) {
+            searchSourceBuilder.query(QueryBuilders.prefixQuery(field, keyword));
+        } else if (option.equals(Option.SUFFIX.get())) {
+            searchSourceBuilder.query(QueryBuilders.wildcardQuery(field, "*" + keyword));
+        } else if (option.equals(Option.CONTAINS.get())) {
+            searchSourceBuilder.query(QueryBuilders.wildcardQuery(field, "*" + keyword + "*"));
+        } else {
+            throw new IllegalArgumentException("정확한 option을 입력하세요.");
+        }
+    }
+}
